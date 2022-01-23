f21e4202282dea7e9158e3885faef8351b49ec7d
Anthony Salemo <ajssalemo@gmail.com>
2022-01-23 15:29:36 -0500

    updated the java example to pull from a envvar for file dir location or use a default value

--- java/src/main/java/com/language/iotests/Controllers/AppendFileController.java
+++ java/src/main/java/com/language/iotests/Controllers/AppendFileController.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppendFileController {
+    @Value("${FILE_DIR:Files}")
+    private String dir;
 
     @GetMapping("/file/append")
     public ResponseEntity<String> appendFile() {
@@ -20,12 +23,12 @@ public class AppendFileController {
         final String fileName = "log-append.txt";
         String content = "This logfile is appended with id: " + id + "\n";
         try {
-            File directory = new File("Files");
+            File directory = new File(dir);
             File file = new File(directory.getAbsolutePath() + File.separator + fileName);
             try (Writer writer = new BufferedWriter(new FileWriter(file, true))) {
                 writer.write(content);
                 return ResponseEntity.status(200).body("Appending to log-append.txt");
-            } 
+            }
         } catch (IOException e) {
             System.out.println(e.toString());
             return ResponseEntity.status(500).body(e.toString());
--- java/src/main/java/com/language/iotests/Controllers/CreateFileController.java
+++ java/src/main/java/com/language/iotests/Controllers/CreateFileController.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CreateFileController {
+    @Value("${FILE_DIR:Files}")
+    private String dir;
 
     @GetMapping("/file/create")
     public ResponseEntity<String> createFile() {
@@ -20,7 +23,7 @@ public class CreateFileController {
         final String fileName = "log-rotate-" + id + ".txt";
         String content = "This is a logfile written with id: " + id;
         try {
-            File directory = new File("Files");
+            File directory = new File(dir);
             File file = new File(directory.getAbsolutePath() + File.separator + fileName);
             try (Writer writer = new BufferedWriter(new FileWriter(file))) {
                 writer.write(content);

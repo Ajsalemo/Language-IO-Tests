ff5e8a8b13360d2ce55feb57af29a9d7f3a3830d
Anthony Salemo <ajssalemo@gmail.com>
2022-01-24 20:32:11 -0500

    able to read files in the Java example

--- java/src/main/java/com/language/iotests/Controllers/ReadFileController.java
+++ java/src/main/java/com/language/iotests/Controllers/ReadFileController.java
@@ -1,11 +1,9 @@
 package com.language.iotests.Controllers;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.Writer;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -13,21 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AppendFileController {
+public class ReadFileController {
     @Value("${FILE_DIR:Files}")
     private String dir;
+    private Object data;
+    String line;
 
-    @GetMapping("/file/append")
-    public ResponseEntity<String> appendFile() {
-        final String id = UUID.randomUUID().toString();
+    @GetMapping("/file/read")
+    public ResponseEntity<Object> readFile() {
         final String fileName = "log-append.txt";
-        String content = "This logfile is appended with id: " + id + "\n";
+        StringBuilder content = new StringBuilder();
         try {
             File directory = new File(dir);
             File file = new File(directory.getAbsolutePath() + File.separator + fileName);
-            try (Writer writer = new BufferedWriter(new FileWriter(file, true))) {
-                writer.write(content);
-                return ResponseEntity.status(200).body("Appending to log-append.txt");
+            try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+                data = content.toString();
+                return ResponseEntity.status(200).body(data);
             }
         } catch (IOException e) {
             e.printStackTrace();
